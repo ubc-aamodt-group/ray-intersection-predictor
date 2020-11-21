d71eea2aedf94068d0ddd2fd269cb935706d8a7d
Lucy Liu <lucy@aamodt-pc18.ece.ubc.ca>
2020-11-21 00:31:28 -0800

    fixed lru time to use tot_sim_cycles

--- src/gpgpu-sim/ray-predictor.cc
+++ src/gpgpu-sim/ray-predictor.cc
@@ -144,7 +144,7 @@ bool ray_predictor::check_table(unsigned long long hash, unsigned long long &ind
     
     // Update LRU
     if (hit && m_placement_policy == 'a' && m_replacement_policy == 'l') {
-      m_predictor_table[hash].m_timestamp = GPGPU_Context()->the_gpgpusim->g_the_gpu->gpu_sim_cycle;
+      m_predictor_table[hash].m_timestamp = GPGPU_Context()->the_gpgpusim->g_the_gpu->gpu_tot_sim_cycle;
     }
     
     return hit;
@@ -159,7 +159,7 @@ bool ray_predictor::check_table(unsigned long long hash, unsigned long long &ind
       if (m_predictor_table[i + way*m_table_size/m_ways].m_valid && m_predictor_table[i + way*m_table_size/m_ways].m_tag == hash) {
         // Update LRU
         if (m_replacement_policy == 'l') {
-          m_predictor_table[i + way*m_table_size/m_ways].m_timestamp = GPGPU_Context()->the_gpgpusim->g_the_gpu->gpu_sim_cycle;
+          m_predictor_table[i + way*m_table_size/m_ways].m_timestamp = GPGPU_Context()->the_gpgpusim->g_the_gpu->gpu_tot_sim_cycle;
         }
         // Update index
         index = i + way*m_table_size/m_ways;
@@ -195,6 +195,7 @@ void ray_predictor::add_entry(unsigned long long hash, new_addr_type predict_nod
           
           num_entry_overflow++;
         }  
+        m_predictor_table[index].m_timestamp = GPGPU_Context()->the_gpgpusim->g_the_gpu->gpu_tot_sim_cycle;
         return;
       }
       else {
@@ -217,7 +218,7 @@ void ray_predictor::add_entry(unsigned long long hash, new_addr_type predict_nod
         }
         num_entry_overflow++;
       }
-      m_predictor_table[hash].m_timestamp = GPGPU_Context()->the_gpgpusim->g_the_gpu->gpu_sim_cycle;
+      m_predictor_table[hash].m_timestamp = GPGPU_Context()->the_gpgpusim->g_the_gpu->gpu_tot_sim_cycle;
       return;
     }
   }
@@ -239,7 +240,7 @@ void ray_predictor::add_entry(unsigned long long hash, new_addr_type predict_nod
           }
           num_entry_overflow++;
         }
-        m_predictor_table[index + way*m_table_size/m_ways].m_timestamp = GPGPU_Context()->the_gpgpusim->g_the_gpu->gpu_sim_cycle;
+        m_predictor_table[index + way*m_table_size/m_ways].m_timestamp = GPGPU_Context()->the_gpgpusim->g_the_gpu->gpu_tot_sim_cycle;
         return;
       }
     }
@@ -248,7 +249,7 @@ void ray_predictor::add_entry(unsigned long long hash, new_addr_type predict_nod
   // Otherwise, create new entry
   predictor_entry new_entry;
   new_entry.m_tag = hash;
-  new_entry.m_timestamp = GPGPU_Context()->the_gpgpusim->g_the_gpu->gpu_sim_cycle;
+  new_entry.m_timestamp = GPGPU_Context()->the_gpgpusim->g_the_gpu->gpu_tot_sim_cycle;
   new_entry.m_valid = true;
   new_entry.m_nodes.push_back(predict_node);
   
@@ -267,7 +268,7 @@ void ray_predictor::add_entry(unsigned long long hash, new_addr_type predict_nod
     unsigned index = hash & (m_table_size/m_ways - 1);
     // Choose way
     unsigned w;
-    unsigned long long lru = GPGPU_Context()->the_gpgpusim->g_the_gpu->gpu_sim_cycle + 1;
+    unsigned long long lru = GPGPU_Context()->the_gpgpusim->g_the_gpu->gpu_tot_sim_cycle + 1;
     bool evict = true;
     for (unsigned way=0; way<m_ways; way++) {
       if (!m_predictor_table[index + way*m_table_size/m_ways].m_valid) {
@@ -294,7 +295,7 @@ void ray_predictor::add_entry(unsigned long long hash, new_addr_type predict_nod
 void ray_predictor::evict_entry() {
   // LRU
   if (m_replacement_policy == 'l') {
-    unsigned long long lru = GPGPU_Context()->the_gpgpusim->g_the_gpu->gpu_sim_cycle + 1;
+    unsigned long long lru = GPGPU_Context()->the_gpgpusim->g_the_gpu->gpu_tot_sim_cycle + 1;
     unsigned long long evicted_hash;
     for (auto it=m_predictor_table.begin(); it!=m_predictor_table.end(); ++it) {
       if (it->second.m_timestamp < lru) {
@@ -302,7 +303,7 @@ void ray_predictor::evict_entry() {
         lru = it->second.m_timestamp;
       }
     }
-    assert(lru != (GPGPU_Context()->the_gpgpusim->g_the_gpu->gpu_sim_cycle + 1));
+    assert(lru != (GPGPU_Context()->the_gpgpusim->g_the_gpu->gpu_tot_sim_cycle + 1));
     m_predictor_table.erase(evicted_hash);
   }
   num_evicted++;
